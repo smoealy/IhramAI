f859c638fe48be22190b3aaca5ca997ddba6293f
smoealy <129239220+smoealy@users.noreply.github.com>
2025-04-30 09:51:39 -0400

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,19 +1,18 @@
-// firebase/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZLugtBQK8eSKdoS2wcZlzxQQ8G2g-yR8",
   authDomain: "ihram-ai.firebaseapp.com",
   projectId: "ihram-ai",
-  storageBucket: "ihram-ai.firebasestorage.app",
+  storageBucket: "ihram-ai.appspot.com",
   messagingSenderId: "486268863536",
   appId: "1:486268863536:web:35f62a29794ab934f519f5",
   measurementId: "G-Y9DZ0332T4"
 };
 
-// Fix: initializeApp() must only be called once
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+export const db = getFirestore(app);
+export const storage = getStorage(app);
